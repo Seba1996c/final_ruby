7e1a1bc4186aeff53a431adc42e560be4928848d
Seba1996c <sebacrash.f2@gmail.com>
2023-07-21 20:26:01 -0400

    segunda base

---
+++ db/migrate/20230722002453_create_job_postings.rb
@@ -0,0 +1,11 @@
+class CreateJobPostings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :job_postings do |t|
+      t.string :title
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
